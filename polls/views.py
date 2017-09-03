66853333f6f56ccc8c50378f9efc0872419f2cd8
maddymaster <maddymaster@gmail.com>
2017-09-03 23:08:33 +0530

    namespace and results

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render, get_object_or_404
 
+
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Question
 
@@ -17,7 +18,17 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-    return HttpResponse("These are the results of the question: %s" % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("Vote on question: %s " % question_id)
+    question = get_object_or_404 (Question, pk = question_id )
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "Please select a choice"})
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
